2e807c9568d4f8282c14ecda664bc2001c6fe45a
swatidurgekar <swatidurgekar@gmail.com>
2023-06-06 23:22:47 +0530

    Editing and Deleting Expenses

--- src/Components/AddExpenses.js
+++ src/Components/AddExpenses.js
@@ -1,24 +1,51 @@
 import "./AddExpenses.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import axios from "axios";
+import { AuthContext } from "./Store/AuthContext";
 
 const AddExpenses = () => {
   const url = "https://react-expense-tracker-e1978-default-rtdb.firebaseio.com";
   const money = useRef();
   const description = useRef();
   const category = useRef();
-  const [expenses, setExpenses] = useState([]);
+  // const [expenses, setExpenses] = useState([]);
+  // const [editing, setEditing] = useState(false);
+  const authCtx = useContext(AuthContext);
 
-  useEffect(() => {
-    async function getExpenses() {
-      const res = await axios.get(`${url}/expenses.json`);
-      const expenses = Object.values(res.data);
-      setExpenses(expenses);
-    }
-    getExpenses();
-  }, []);
+  // useEffect(() => {
+  //   async function getExpenses() {
+  //     const res = await axios.get(`${url}/expenses.json`);
+  //     const expenses = Object.values(res.data);
+  //     setExpenses(expenses);
+  //   }
+  //   getExpenses();
+  // }, [expenses]);
+
+  const deleteExpense = async (item) => {
+    const res = await axios.get(`${url}/expenses.json`);
+    const keys = Object.keys(res.data);
+    keys.map((expenseKey) => {
+      if (res.data[expenseKey].enteredDesc === item.enteredDesc) {
+        axios.delete(`${url}/expenses/${expenseKey}.json`);
+      }
+    });
+  };
+
+  const editExpense = async (item) => {
+    authCtx.edit();
+    money.current.value = item.enteredMoney;
+    description.current.value = item.enteredDesc;
+    category.current.value = item.enteredCategory;
+    const res = await axios.get(`${url}/expenses.json`);
+    const keys = Object.keys(res.data);
+    keys.map((expenseKey) => {
+      if (res.data[expenseKey].enteredDesc === item.enteredDesc) {
+        authCtx.editKey(expenseKey);
+      }
+    });
+  };
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -27,20 +54,14 @@ const AddExpenses = () => {
     const enteredCategory = category.current.value;
     const newExpense = { enteredMoney, enteredDesc, enteredCategory };
 
-    axios.post(`${url}/expenses.json`, newExpense).then((res) => {
-      if (res.statusText) {
-        setExpenses((prevExpense) => {
-          return [...prevExpense, newExpense];
-        });
-      }
-    });
+    authCtx.addExpense(newExpense);
   };
 
   return (
     <div className="add-expenses-div">
       <h6 className="add-expenses-header">Add income and expenses daily</h6>
       <div className="add-expenses-form">
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3">
             <label>Money spent</label>
             <Form.Control ref={money} type="text" required />
@@ -52,24 +73,40 @@ const AddExpenses = () => {
           <Form.Group className="mb-3">
             <label>Category</label>
             <Form.Select ref={category}>
+              <option>Select category</option>
               <option value="grocery">Grocery</option>
               <option value="food">Food</option>
               <option value="petrol">Petrol</option>
             </Form.Select>
           </Form.Group>
-          <Button onClick={submitHandler} type="submit">
-            Add expense
-          </Button>
+          <Button type="submit">Add expense</Button>
         </Form>
       </div>
-      {expenses && (
+      {authCtx.expenses && (
         <div className="expenses">
           <h5>Expenses</h5>
           <hr />
-          {expenses.map((item) => {
+          {authCtx.expenses.map((item) => {
             return (
               <div key={item.enteredDesc}>
-                <li>{`${item.enteredMoney}$ ${item.enteredDesc} ${item.enteredCategory}`}</li>
+                <li>
+                  {`${item.enteredMoney}$ ${item.enteredDesc} ${item.enteredCategory}`}
+                  <button
+                    onClick={() => {
+                      editExpense(item);
+                    }}
+                    className="expense-edit-button"
+                  >
+                    EDIT
+                  </button>
+                  <button
+                    onClick={() => deleteExpense(item)}
+                    className="expense-delete-button"
+                  >
+                    DELETE
+                  </button>
+                </li>
+
                 <hr />
               </div>
             );
--- src/Components/Store/AuthContext.js
+++ src/Components/Store/AuthContext.js
@@ -1,16 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const AuthContext = createContext({
   islogin: "",
   isLoggedIn: "",
   idToken: "",
+  editing: "",
+  editingKey: "",
   logout: () => {},
+  addExpense: () => {},
+  edit: () => {},
 });
 
 export const AuthContextProvider = (props) => {
+  const url = "https://react-expense-tracker-e1978-default-rtdb.firebaseio.com";
   const token = localStorage.getItem("idToken");
+  const [editing, setEditing] = useState(false);
+  const [editingKey, setEditingKey] = useState("");
   const [islogin, setIsLogin] = useState();
   const [idToken, setIdToken] = useState(token);
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const res = await axios.get(`${url}/expenses.json`);
+      if (res.data) {
+        const expenses = Object.values(res.data);
+        setExpenses(expenses);
+      }
+    }
+    getExpenses();
+  }, [expenses]);
 
   const login = () => {
     setIsLogin(true);
@@ -21,11 +41,35 @@ export const AuthContextProvider = (props) => {
     setIdToken("");
   };
 
+  const addExpense = (expense) => {
+    {
+      editing && axios.put(`${url}/expenses/${editingKey}.json`, expense);
+    }
+    {
+      !editing && axios.post(`${url}/expenses.json`, expense);
+    }
+    setEditing(false);
+  };
+
+  const edit = () => {
+    setEditing(true);
+  };
+
+  const editKey = (key) => {
+    setEditingKey(key);
+  };
+
   const authContext = {
+    expenses: expenses,
     islogin: !!idToken,
     isLoggedIn: login,
     idToken: idToken,
     logout: logoutHandler,
+    addExpense: addExpense,
+    editing: editing,
+    edit: edit,
+    editingKey: editingKey,
+    editKey: editKey,
   };
 
   return (
